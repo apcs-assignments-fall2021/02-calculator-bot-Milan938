8c8f8385fe0f6092dea48899d5fcf440f190478d
milanboga <23mboga@athenian.org>
2021-09-04 14:06:35 -0700

    I don't know why it isn't working properly, it asks the user which command they'd like to do but just stops after that

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,27 +4,66 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (a > b && a > c || a < b && a > c) {
+            return a;
+        }
+        else if (b > a && b < c || b > c && b < a) {
+            return b;
+        }
+        else if (c > a && c < b || c < a && c > b) {
+            return c;
+        }
+        return 0;
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        }
+        else if (Math.abs(b) > Math.abs(a)) {
+            return b;
+        }
+        return 0;
     }
+
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        double y = a*a;
+        double z = b*b;
+        double k = (y + z);
+        double u = (Math.sqrt(k));
+        return u;
     }
 
     public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
-
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.print("Which command would you like to carry out?");
+        String answer = scan.next();
+        if (answer == ("median")) {
+            System.out.print("What's the first number?");
+            int h = scan.nextInt();
+            System.out.print("What's the second number?");
+            int j = scan.nextInt();
+            System.out.print("What's the third number?");
+            int k = scan.nextInt();
+            System.out.print(median(h, j, k));
+        }
+        if (answer == "magnitude") {
+            System.out.print("What's the first number?");
+            int z = scan.nextInt();
+            System.out.print("What's the second number?");
+            int x = scan.nextInt();
+            System.out.print(magnitude(z, x));
+        }
+        if (answer == "pythagoras") {
+            System.out.print("What's the first number?");
+            int i = scan.nextInt();
+            System.out.print("What's the second number?");
+            int o = scan.nextInt();
+            System.out.print(pythagoras(i, o));
+        }
     }
 }
